7d4aa5aefec4437fae8f87b1b51e6e3698687d1b
henhenrys <tranhenry94@gmail.com>
2023-01-29 23:52:39 -0800

    fix

--- SearchEngine.java
+++ SearchEngine.java
@@ -9,6 +9,7 @@ class Handler implements URLHandler {
     ArrayList<String> returned = new ArrayList<String>();
     String returns = "";
     int i = 0;
+    int t = 0;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
@@ -17,7 +18,7 @@ class Handler implements URLHandler {
         else if (url.getPath().equals("/search")) {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("t")) {
-                for(int t = 0; t < returned.size(); t++) {
+                for(t = t; t < returned.size(); t++) {
                     if(returned.get(t).contains(parameters[1])) {
                         returns = returns + returned.get(t) + "\n";
                     }
