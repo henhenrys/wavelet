346ee1c14c317954962a5800c752785134fac0de
henhenrys <tranhenry94@gmail.com>
2023-01-29 22:34:39 -0800

    yes

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,24 +1,38 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    int num = 0;
+    String returning = "";
+    ArrayList<String> returned = new ArrayList<String>();
+    String returns = "";
+    int i = 0;
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return String.format("Henry's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
+            return String.format("%s", returning);
+        } 
+        else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("t")) {
+                for(int t = 0; t < returned.size(); t++) {
+                    if(returned.get(t).contains(parameters[1])) {
+                        returns = returns + returned.get(t) + "\n";
+                    }
+                }
+            }
+            return returns;
+        }
+        else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    returning = returning + parameters[1] + '\n';
+                    returned.add(parameters[1]);
+                    return String.format("The String, %s ,has been added!", parameters[1]);
                 }
             }
             return "404 Not Found!";
@@ -26,7 +40,7 @@ class Handler implements URLHandler {
     }
 }
 
-class SearchEnginer {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
